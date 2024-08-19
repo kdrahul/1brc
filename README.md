4cdddd25786e4170fa36ca5a9129f557b1b14d0f
Rahul Kulkarni <kdrahul@github.com>
2024-08-19 17:27:16 +0530

    Removed most vec allocations

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # 1brc
 
 The One-Billion-Row Challenge attempt, in Rust.
+
+## Records
+
+| What I did | Commit | Time | Graph |
+| -----------| -------| ------| ------- |
+| Removed as many vecs as possible | | 200s| [Flamegraph](flamegraphs/removing_vecs.svg) |
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,75 @@
+use rayon::prelude::*;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, BufWriter, Write};
+use std::ops::Add;
 
 #[derive(Debug)]
 struct Data {
-    city: String,
-    temp: f32,
+    min: f32,
+    max: f32,
+    sum: f32,
+    count: u32,
 }
 
-fn process_data(s: &str) -> Data {
-    let splits: Vec<&str> = s.split(";").collect();
-    Data {
-        city: splits.get(0).unwrap().to_string(),
-        temp: splits.get(1).unwrap().parse::<f32>().unwrap(),
+impl Data {
+    fn update(&mut self, temp: f32) {
+        self.min = self.min.min(temp);
+        self.max = self.max.max(temp);
+        self.sum = self.sum.add(temp);
+        self.count += 1;
     }
 }
 
-fn find_values(v: &Vec<f32>) -> [f32; 3] {
-    let mut min: f32 = 0.0;
-    let mut sum: f32 = 0.0;
-    let mut max: f32 = 0.0;
-
-    for i in 0..v.len() {
-        min = min.min(v[i]);
-        max = max.max(v[i]);
-        sum += v[i];
-    }
-    [min, sum / v.len() as f32, max]
+fn process_data(s: &str) -> (String, f32) {
+    let mut splits = s.split(";");
+    (
+        splits.next().expect("error").to_string(),
+        splits.next().expect("error").parse::<f32>().unwrap(),
+    )
 }
 
 fn main() {
+    // Open the file
     let file = File::open("measurements.txt").unwrap();
-    // Read all lines of the file
-    let mut iter = io::BufReader::new(file)
-        .lines()
-        .map(|l| l.unwrap());
-    let mut map: HashMap<String, Vec<f32>> = HashMap::new();
+    // Read all lines of the file.
+    // FIXME: Reads all 14GB into ram. Change this to store few lines at a time
+    let mut iter = io::BufReader::new(file).lines().map(|l| l.unwrap());
 
+    let mut map: HashMap<String, Data> = HashMap::new();
 
     while let Some(t) = iter.next() {
-        let d = process_data(&t);
-        map.entry(d.city)
-            .and_modify(|f| f.push(d.temp))
-            .or_insert(vec![d.temp]);
+        let (name, temp) = process_data(&t);
+        map.entry(name)
+            .and_modify(|f| f.update(temp))
+            .or_insert(Data {
+                min: temp,
+                max: temp,
+                sum: temp,
+                count: 1,
+            });
     }
 
-    let mut final_string: String = String::new();
-    final_string.push('{');
+    let mut writer = BufWriter::new(std::io::stdout());
+    let _ = writer.write_all(b"{");
 
-    let all_strs :Vec<String> = map.iter().map(|(key,value)|{
-        let min: f32;
-        let max: f32;
-        let avg: f32;
+    let len = map.len();
+    let mut count: usize = 0;
 
-        let res: [f32; 3] = find_values(value);
+    map.iter().for_each(|(key, value)| {
+        let min: f32 = value.min;
+        let max: f32 = value.max;
+        let avg: f32 = value.sum / value.count as f32;
 
-        min = res[0];
-        avg = res[1];
-        max = res[2];
-        format!("{}={:.1}/{:.1}/{:.1}", &key, min, avg, max)
-    }).collect();
+        count += 1;
+        let s = &format!("{}={:.1}/{:.1}/{:.1}", &key, min, avg, max);
+        writer.write_all(s.as_bytes()).unwrap();
+        if len > count {
+            writer.write_all(b", ").unwrap();
+        }
+    });
 
-    final_string.push_str(&all_strs.join(", "));
-    final_string = final_string[0..final_string.len() - 2].to_string();
-    final_string.push('}');
-    println!("{}", final_string);
+
+    writer.write_all(b"}").unwrap();
+    writer.flush().unwrap();
 }
